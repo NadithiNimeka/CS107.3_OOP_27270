f4e09aec0cbd66a11cc3ec65695ced79f935e3b7
NadithiNimeka <137481854+NadithiNimeka@users.noreply.github.com>
2023-07-28 10:49:26 +0530

    Add files via upload

---
+++ Lab01-20230512/Q1/Q1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Your batch: ");
+            int batch=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hii! "+ name);
+            Console.WriteLine("your batch is " + batch);    
+        }
+    }
+}
