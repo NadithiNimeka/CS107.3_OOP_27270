60111f162c2d4aaeeccac4a8fc8468814ce1ffdb
NadithiNimeka <137481854+NadithiNimeka@users.noreply.github.com>
2023-07-28 11:20:34 +0530

    Add files via upload

---
+++ Lab01-20230512/Q2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            double a = 3.14 * r * r;
+            Console.WriteLine("Area of the Circle: "+ a);
+
+        }
+    }
+}
---
+++ Lab01-20230512/Q4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your salary: ");
+            int sal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your tax rate: ");
+            int taxrate = Convert.ToInt32(Console.ReadLine());
+
+            int tax = (taxrate*sal1)/100;
+            int sal2 = sal1 - tax ;
+
+            Console.WriteLine("Your salary after tax is "+ sal2);
+        }
+    }
+}
